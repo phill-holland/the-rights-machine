127eff544123319b7264a29ff6ae728915978a6c
Phill Holland <fotofill.holland@googlemail.com>
2018-10-14 21:56:39 +0100

    various updates

---
+++ process/tests/error.cpp
@@ -0,0 +1,42 @@
+#include "stdafx.h"
+#include "error.h"
+
+tests::data::error::error()
+{
+	json *identifiers[] = { this };
+
+	for (long i = 0L; i < 1L; ++i)
+	{
+		hash[identifiers[i]->FQDN()] = identifiers[i];
+	}
+}
+
+bool tests::data::error::add(custom::pair &source)
+{
+	if (string("code").icompare(source.name))
+	{
+		code = source.value.toLong();
+	}
+	if (string("name").icompare(source.name))
+	{
+		name = source.value;
+
+		return true;
+	}
+	else if (string("description").icompare(source.name))
+	{
+		description = source.value;
+
+		return true;
+	}
+	
+	return false;
+}
+
+::data::json::request::json *tests::data::error::find(string FQDN)
+{
+	std::unordered_map<string, json*, comparison::hasher, comparison::equality>::iterator i = hash.find(FQDN);
+	if (i != hash.end()) return (json*)i->second;
+
+	return NULL;
+}
---
+++ process/tests/error.h
@@ -0,0 +1,42 @@
+#include "../process/json.h"
+#include "../process/pair.h"
+#include "../process/string.h"
+#include "../process/datetime.h"
+#include "../process/comparison.h"
+#include <unordered_map>
+
+#if !defined(__TEST_RESULT)
+#define __TEST_RESULT
+
+namespace tests
+{
+	namespace data
+	{
+		using namespace global;
+
+		class error : public ::data::json::request::json
+		{
+			std::unordered_map<string, json *, comparison::hasher, comparison::equality> hash;
+
+		public:
+			long code;
+			string name;
+			string description;
+
+		public:
+			error();
+
+		public:
+			bool add(custom::pair &source);
+
+			int identity() { return 0; }
+			void clear()  { }
+			string identifier() { return string("error"); }
+
+		public:
+			json *find(string FQDN);
+		};
+	};
+};
+
+#endif
